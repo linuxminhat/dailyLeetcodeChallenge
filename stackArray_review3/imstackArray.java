91c5a9ed3e3154ad357583f24c357f04febfdbd1
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-07-22 19:40:13 +0700

    continue learning

---
+++ stackArray_review3/imstackArray.java
@@ -0,0 +1,62 @@
+package stackArray_review3;
+
+public class imstackArray implements istackArray {
+	int[] newArray;
+	int SIZE;
+	int topIndex;
+	imstackArray(int size){
+		SIZE=size;
+		newArray = new int[SIZE];
+		topIndex=-1;
+	}
+
+	@Override
+	public boolean push(int value) {
+		if(!isFull()) {
+			topIndex++;
+			newArray[topIndex]=value;
+			return true;
+		}
+		 
+		return false;
+	}
+
+	@Override
+	public int pop() {
+		if(!isEmpty()) {
+			int value = newArray[topIndex];
+			topIndex--;
+			return value;
+		}
+		return -1;
+	}
+
+	@Override
+	public boolean isFull() {
+		 
+		return topIndex==SIZE-1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+	 
+		return topIndex<0;
+	}
+
+	@Override
+	public void printing() {
+		if(isEmpty()) {
+			System.out.println("This array is empty ! ");
+		}else {
+			for(int i=0;i<=topIndex;i++) {
+				System.out.println(newArray[topIndex]);
+			}
+				System.out.println();
+			
+		}
+		 
+		
+	}
+	
+
+}
