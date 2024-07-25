349288976f0ab7539e2854953ff87cd23724c3cf
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-07-26 01:15:36 +0700

    continue

--- hashTable/myHashSet_learning7.java
+++ hashTable/myHashSet_learning7.java
@@ -2,36 +2,41 @@ package hashTable;
 
 import java.util.ArrayList;
 
-public class myHashSet_learning6 {
+public class myHashSet_learning7 {
 	public final int SIZE=1000;
 	ArrayList<Integer> myBucket[];
-	//declare a constructor 
-	myHashSet_learning6(){
+	//declare constructor 
+	myHashSet_learning7(){
 		myBucket = new ArrayList[SIZE];
 		for(int i=0;i<myBucket.length;i++) {
 			myBucket[i]=new ArrayList<>();
 		}
 	}
-	
 	//implementing all function 
 	public int hashFunction(int key) {
 		return key%SIZE;
 	}
-	
 	public void addFunction(int key) {
-		int hashValue = hashFunction(key);
-		var bucket = myBucket[hashValue];
+		int hashKeyValue = hashFunction(key);
+		var bucket = myBucket[hashKeyValue];
 		int keyIndexValue = bucket.indexOf(key);
 		if(keyIndexValue<0) {
 			bucket.add(key);
 		}
 	}
 	public void removeFunction(int key) {
-		int hashValue = hashFunction(key);
-		var bucket = myBucket[hashValue];
+		int hashKeyValue = hashFunction(key);
+		var bucket = myBucket[hashKeyValue];
 		int keyIndexValue = bucket.indexOf(key);
 		if(keyIndexValue>=0) {
 			bucket.remove(keyIndexValue);
-		}
+		}	
 	}
+	public boolean chekingAvailable(int key) {
+		int hashKeyValue = hashFunction(key);
+		var bucket = myBucket[hashKeyValue];
+		int keyIndexValue = bucket.indexOf(key);
+		return keyIndexValue>=0;
+	}
+
 }
