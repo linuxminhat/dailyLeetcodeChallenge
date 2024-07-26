312cf6fef84dc8188854d03b3457a68a914b1831
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-07-26 20:49:00 +0700

    review hashfunction

--- hashTable/myHashMap_learning6.java
+++ hashTable/myHashMap_learning6.java
@@ -2,10 +2,10 @@ package hashTable;
 
 import java.util.ArrayList;
 
-public class myHashMap_learning5 {
-	private final int SIZE=1000;
-	//implementing a new data type 
-	class Data{
+public class myHashMap_learning6 {
+	private static final int SIZE = 1000;
+	ArrayList<Data> myBucket[];
+	public class Data{
 		int key;
 		int value;
 		Data(int key,int value){
@@ -15,36 +15,35 @@ public class myHashMap_learning5 {
 		@Override
 		public boolean equals(Object obj) {
 			if(obj instanceof Data) {
-				return key==(((Data)obj).key);
+				return key==((Data)obj).value;
+				
 			}
 			return false;
 		}
 	}
-	
-	ArrayList<Data> myBucket[];
-	myHashMap_learning5(){
+	myHashMap_learning6(){
 		myBucket=new ArrayList[SIZE];
 		for(int i=0;i<myBucket.length;i++) {
-			myBucket[i]=new ArrayList<>();
+			myBucket[i]= new ArrayList<>();
 		}
 	}
-
-	//implementing all needed function
-	
+	//implementing all needed function 
 	public int hashFunction(int key) {
 		return key%SIZE;
+		
 	}
 	public void put(int key,int value) {
 		int hashValueIndex = hashFunction(key);
 		var bucket = myBucket[hashValueIndex];
-		Data tempData = new Data(key,0);//Chua duoc luu trong bucket 
+		Data tempData = new Data(key,0);
 		int keyIndexValue = bucket.indexOf(tempData);
 		if(keyIndexValue>=0) {
 			bucket.get(keyIndexValue).value=value;
 		}else {
 			bucket.add(tempData);
+			//bucket.add(key)=> cant
+			
 		}
-		
 	}
 	public void remove(int key) {
 		
@@ -52,7 +51,5 @@ public class myHashMap_learning5 {
 	public int get(int key) {
 		return 0;
 	}
-	
-	
 
 }
--- hashTable/myHashMap_learning7.java
+++ hashTable/myHashMap_learning7.java
@@ -2,9 +2,9 @@ package hashTable;
 
 import java.util.ArrayList;
 
-public class myHashMap_learning5 {
-	private final int SIZE=1000;
-	//implementing a new data type 
+public class myHashMap_learning7 {
+	private static final int SIZE = 1000;
+	ArrayList<Data> myBucket[];
 	class Data{
 		int key;
 		int value;
@@ -15,36 +15,33 @@ public class myHashMap_learning5 {
 		@Override
 		public boolean equals(Object obj) {
 			if(obj instanceof Data) {
-				return key==(((Data)obj).key);
+				return key==((Data)obj).key;	
 			}
 			return false;
 		}
 	}
-	
-	ArrayList<Data> myBucket[];
-	myHashMap_learning5(){
+	myHashMap_learning7(){
 		myBucket=new ArrayList[SIZE];
 		for(int i=0;i<myBucket.length;i++) {
 			myBucket[i]=new ArrayList<>();
 		}
 	}
-
-	//implementing all needed function
-	
+	//implementing all needed function 
 	public int hashFunction(int key) {
 		return key%SIZE;
+		
 	}
 	public void put(int key,int value) {
-		int hashValueIndex = hashFunction(key);
-		var bucket = myBucket[hashValueIndex];
-		Data tempData = new Data(key,0);//Chua duoc luu trong bucket 
+		int hashKeyValue = hashFunction(value);
+		var bucket = myBucket[hashKeyValue];
+//		int keyIndexValue = bucket.indexOf(key); => Not true 
+		Data tempData = new Data(key,0);
 		int keyIndexValue = bucket.indexOf(tempData);
 		if(keyIndexValue>=0) {
 			bucket.get(keyIndexValue).value=value;
 		}else {
 			bucket.add(tempData);
 		}
-		
 	}
 	public void remove(int key) {
 		
@@ -52,7 +49,5 @@ public class myHashMap_learning5 {
 	public int get(int key) {
 		return 0;
 	}
-	
-	
 
 }
