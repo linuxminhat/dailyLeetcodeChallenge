3feafed9e0f607c18888ec6064ce17d8a1cf1730
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-07-29 22:53:16 +0700

    done bst

---
+++ binarySearchTree_learning20/Solution.java
@@ -0,0 +1,30 @@
+package binarySearchTree_learning20;
+
+
+ class TreeNode {
+ int val;
+ TreeNode left;
+ TreeNode right;
+ TreeNode() {}
+ TreeNode(int val) { this.val = val; }
+ TreeNode(int val, TreeNode left, TreeNode right) {
+ this.val = val;
+ this.left = left;
+ this.right = right;
+ }
+}
+
+
+ class Solution {
+	    public boolean hasPathSum(TreeNode root, int targetSum) {
+	    	if(root==null) {
+	    		return false;//Cay rong khong co duong di 
+	    	}
+	    	if(root.left==null && root.right==null) {
+	    		return root.val==targetSum;
+	    	}
+	    	int newTargetSum =  targetSum-root.val;
+	    	return hasPathSum(root.right,newTargetSum) || hasPathSum(root.right,newTargetSum);
+	        
+	    }
+	}
