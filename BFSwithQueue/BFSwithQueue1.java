2f12131ea5ce1eb6baab26cbc8a1f52a87aa710c
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-08-09 09:37:35 +0700

    DONE BFS

---
+++ BFSwithQueue/BFSwithQueue1.java
@@ -0,0 +1,34 @@
+package BFSwithQueue;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class BFSwithQueue1 {
+	public static void main(String[] args) {
+		int[][] graph = {
+				{0,1,0,0,0,0,0},
+				{1,0,1,1,1,0,0},
+				{0,1,0,0,0,1,0},
+				{0,1,0,0,0,1,1},
+				{0,1,0,0,0,0,1},
+				{0,0,1,1,0,0,0},
+				{0,0,0,1,1,0,0}};
+		Queue<Integer> myQueue = new LinkedList<>();
+		myQueue.add(0);
+		Set<Integer> daDuyet = new HashSet<>();
+		daDuyet.add(0);
+		while(!myQueue.isEmpty()) {
+			int u = myQueue.poll();
+			System.out.println(u + "");
+			for(int v=0;v<graph.length;v++) {
+				if(graph[u][v]==1 && daDuyet.contains(v)==false) {
+					myQueue.add(v);
+					daDuyet.add(v);
+				}
+			}
+		}
+	}
+
+}
