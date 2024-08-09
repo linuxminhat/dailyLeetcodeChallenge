53d2fd39aa8ec2f2fce4aab7103062c673d1b7f5
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-08-09 08:38:44 +0700

    DFS with recursion

---
+++ DFSwithRecursion/DFSwithRecursion4.java
@@ -0,0 +1,31 @@
+package DFSwithRecursion;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DFSwithRecursion4 {
+	public static void DFSwithRecursion(int u, int[][] graph, Set<Integer> daDuyet) {
+		System.out.println(u + "");
+		for(int v=0;v<graph.length;v++) {
+			if(graph[u][v]==1 && daDuyet.contains(v)==false) {
+				daDuyet.add(v);
+				DFSwithRecursion(v,graph,daDuyet);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[][] graph = {
+				{0,1,0,0,0,0,0},
+				{1,0,1,1,1,0,0},
+				{0,1,0,0,0,1,0},
+				{0,1,0,0,0,1,1},
+				{0,1,0,0,0,0,1},
+				{0,0,1,1,0,0,0},
+				{0,0,0,1,1,0,0}};
+		Set<Integer> daDuyet = new HashSet<>();
+		daDuyet.add(0);
+		DFSwithRecursion(0,graph,daDuyet);
+		
+	}
+
+}
