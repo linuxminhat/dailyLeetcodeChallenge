a60c9b5252790c1ddd12155964d38e496de6fe24
dangnhatminhdhbk@gmail.com <dangnhatminhdhbk@gmail.com>
2024-08-09 23:53:14 +0700

    BFS with queue

---
+++ DFSwithStack/leetcode.java
@@ -0,0 +1,22 @@
+package DFSwithStack;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class leetcode {
+	public int numIslands(char[][] grid) {
+		//have a grid 
+		Stack<Integer> myStack = new Stack<>();
+		Set<Integer> daDuyet = new HashSet<>();
+		while(!myStack.isEmpty()) {
+			
+		}
+		
+		return 0;
+	}
+	public static void main(String[] args) {
+		 
+	}
+	
+}
